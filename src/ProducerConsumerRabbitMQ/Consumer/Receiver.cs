2fc13db17e6d96172a122ade2580b89114952532
Matt Klepeis <mattklepeis@gmail.com>
2019-09-30 00:52:34 -0400

    Producer/Consumer working. Using on channel in consumer not working as shown in Demo.

---
+++ src/ProducerConsumerRabbitMQ/Consumer/Receiver.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            // Create a Factory
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            // Create a Connection
+            // Open a Channel
+            using (var connection = factory.CreateConnection())
+            {
+                var channel = connection.CreateModel();
+
+                // Declare the queue
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"Received message {message}...");
+                };
+
+                channel.BasicConsume("BasicTest", true, consumer);
+            }
+                       
+            Console.WriteLine("Press [enter] to exit the Consumer App...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/ProducerConsumerRabbitMQ/Producer/Sender.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            // Create a Factory
+            var factory = new ConnectionFactory() 
+            {
+                HostName = "localhost"
+            };
+
+            // Create a Connection
+            // Open a Channel
+            using (var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                // Declare the queue
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                // Create a message
+                string message = "Getting started with .Net Core RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                // Publish the message
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine($"Sent message {message}...");
+            }
+
+            Console.WriteLine("Press [enter] to exit the Sender App...");
+            Console.ReadLine();
+        }
+    }
+}
