4d3f73255d66a12906322c01fe3fd584830d1f90
Matt Klepeis <mattklepeis@gmail.com>
2019-09-30 20:33:01 -0400

    Updated DependencyContainer

--- src/MicroRabbit/MicroRabbit/MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
+++ src/MicroRabbit/MicroRabbit/MicroRabbit.Infrastructure.IoC/DependencyContainer.cs
@@ -1,9 +1,6 @@
 ﻿using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infrastructure.Bus;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MicroRabbit.Infrastructure.IoC
 {
